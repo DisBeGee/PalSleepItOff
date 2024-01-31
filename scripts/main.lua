27cf578c46027d85e52be2424301d5e03857892b
Gerald Dasal <gerald.dasal@gmail.com>
2024-01-30 23:20:36 -0500

    Adding Utility and Spa functionality

--- scripts/main.lua
+++ scripts/main.lua
@@ -1,6 +1,11 @@
-RegisterHook("Function /Game/Pal/Blueprint/Controller/AIAction/BaseCamp/BP_AIActionBaseCamp_Sleep.BP_AIActionBaseCamp_Sleep_C:ActionStart", function(self, ControlledPawn)
-	local characterActor = self:get():GetOuter():GetOuter().Character
-	
+local utility = StaticFindObject("/Script/Pal.Default__PalUtility")
+
+if not utility:IsValid() then
+	print("Could not find utility! Sleep It Off failed to load!")
+	return
+end
+
+function curePal(characterActor)
 	local palParam = utility:GetIndividualCharacterParameterByActor(characterActor)
 	
 	local catSaveParam = palParam["SaveParameter"]
@@ -11,6 +16,15 @@ RegisterHook("Function /Game/Pal/Blueprint/Controller/AIAction/BaseCamp/BP_AIAct
 			catSaveParam["WorkerSick"] = 0
 		end)
 	end
+
+end
+
+RegisterHook("/Game/Pal/Blueprint/Controller/AIAction/BaseCamp/BP_AIActionBaseCamp_Sleep.BP_AIActionBaseCamp_Sleep_C:ActionStart", function(self, ControlledPawn)
+	curePal(self:get():GetOuter():GetOuter().Character)
+end)
+
+RegisterHook("/Game/Pal/Blueprint/Controller/AIAction/BaseCamp/BP_AIActionBaseCamp_InSpa.BP_AIActionBaseCamp_InSpa_C:ActionStart", function(self, ControlledPawn)
+	curePal(self:get():GetOuter():GetOuter().Character)
 end)
 
 print("Successfully loaded Sleep It Off!")
